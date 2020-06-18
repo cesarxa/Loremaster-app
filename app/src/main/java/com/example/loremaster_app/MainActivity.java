cb94b3bf59b4d18d838bb5dcebc299a59557edfb
cesarxa <cesarxhernandez@protonmail.com>
2020-06-18 16:23:47 -0600

    added scryfall class

--- app/src/main/java/com/example/loremaster_app/MainActivity.java
+++ app/src/main/java/com/example/loremaster_app/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.loremaster_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        data = (TextView) findViewById(R.id,scryfalldata)
     }
     
 }
---
+++ app/src/main/java/com/example/loremaster_app/scryFall.java
@@ -0,0 +1,7 @@
+package com.example.loremaster_app;
+
+import android.os.AsyncTask;
+
+public class scryFall extends AsyncTask <Void,Void,Void>
+{
+}
