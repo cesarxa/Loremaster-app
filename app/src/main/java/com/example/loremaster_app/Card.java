95a64f13a2d648198f288177a25fe7d835bc46e4
JayWheels <111hotwheels@gmail.com>
2020-06-22 10:01:00 -0600

    Lots of changes, did my best to understand code I did not write.

--- app/src/main/java/com/example/loremaster_app/Card.java
+++ app/src/main/java/com/example/loremaster_app/Card.java
@@ -30,7 +30,7 @@ public class Card extends AppCompatActivity {
         getCard = (EditText) findViewById(R.id.getCard);
     }
 
-    public void getCardInfo(View view) {
+    public void getCardInfo() {
         //still need correct info request  !FIX - missing "+ getCard.getText().toString() +" in url.
         //String url = "https://api.scryfall.com/bulk-data/922288cb-4bef-45e1-bb30-0c2bd3d3534f";
         entireURL = "https://api.scryfall.com/cards/named?fuzzy=" + getCard.getText().toString();
