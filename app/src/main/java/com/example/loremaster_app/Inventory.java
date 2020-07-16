b464dd0002d10f1ef6ef3cae6d1234871924fe11
SterlingB9 <SBoelter727@gmail.com>
2020-07-15 18:23:00 -0600

    added getInventoryInfo function in Inventory.java

--- app/src/main/java/com/example/loremaster_app/Inventory.java
+++ app/src/main/java/com/example/loremaster_app/Inventory.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Inventory extends AppCompatActivity {
+
+    private static final String EXTRA_MESSAGE = "iventory.java is doing stuff";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +24,15 @@ public class Inventory extends AppCompatActivity {
     public void finish() {
         super.finish();
     }
+
+    // search list of inventory items for card
+    public void getInventoryInfo(View view) {
+        Intent InventorySearch = new Intent(this, Inventory.class);
+        EditText searchPrefInventory = (EditText) findViewById(R.id.searchPrefInventory);
+        String message = searchPrefInventory.getText().toString();
+        InventorySearch.putExtra(EXTRA_MESSAGE, message);
+        startActivity(InventorySearch);
+
+        }
 
 }
