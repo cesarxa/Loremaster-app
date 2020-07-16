2a2f8acbc1bf7f5f6b4c8ff43730dbf65525ebfb
SterlingB9 <SBoelter727@gmail.com>
2020-07-16 16:31:42 -0600

    added getInventoryItem class to inventory but basically had no idea what i was doing, it doesnt like something but i dont know why

--- app/src/main/java/com/example/loremaster_app/Inventory.java
+++ app/src/main/java/com/example/loremaster_app/Inventory.java
@@ -16,23 +16,35 @@ public class Inventory extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
     }
+
     public void viewCardAct(View view) {
         Intent intent = new Intent(this, Card.class);
         startActivity(intent);
     }
+
     @Override
     public void finish() {
         super.finish();
     }
 
-    // search list of inventory items for card
-    public void getInventoryInfo(View view) {
-        Intent InventorySearch = new Intent(this, Inventory.class);
-        EditText searchPrefInventory = (EditText) findViewById(R.id.searchPrefInventory);
-        String message = searchPrefInventory.getText().toString();
-        InventorySearch.putExtra(EXTRA_MESSAGE, message);
-        startActivity(InventorySearch);
+    /*
+    // get the card the user searches for frim shared preferences and display it
+    public class getInventoryItem implements Runnable {
+        //private String inventoryObject;
 
+        // search list of inventory items for cards and display in logcat
+        public void getInventoryInfo(View view) {
+            Intent InventorySearch = new Intent(this, Inventory.class);
+            EditText searchPrefInventory = (EditText) findViewById(R.id.searchPrefInventory);
+            String message = searchPrefInventory.getText().toString();
+            InventorySearch.putExtra(EXTRA_MESSAGE, message);
+            startActivity(InventorySearch);
+
+            @Override
+            public void run() {
+
+            }
         }
-
+    }
+    */
 }
