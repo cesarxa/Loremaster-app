3e0029537037ce487d41f6b53763f88edc4c6897
JayWheels <111hotwheels@gmail.com>
2020-07-17 17:23:53 -0600

    Doubles of cards can be added to the inventory.

--- app/src/main/java/com/example/loremaster_app/Card.java
+++ app/src/main/java/com/example/loremaster_app/Card.java
@@ -81,23 +81,31 @@ public class Card extends AppCompatActivity {
 
     // add a card, if it already exists then just add to quantity
     public void addCard(View view) {
+        boolean isAClone = false;
+        System.out.println(currentCard.getName());
         if (quantity > 0) {
             for (InventoryItem anItem : inventory) {
                 if (currentCard.getName().equals(anItem.getCardInfo().getName())) {
+                    System.out.println("Made it inside of the if statement");
+                    System.out.println(quantity);
+                    System.out.println(anItem.getQuantity());
                     anItem.setQuantity(anItem.getQuantity() + quantity);
-                } else {
-                    InventoryItem newItem = new InventoryItem();
-                    newItem.setCardInfo(currentCard);
-                    newItem.setQuantity(quantity);
-                    inventory.add(newItem);
-                    SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
-                    SharedPreferences.Editor editor = pref.edit();
-                    Gson gson2 = new Gson();
-                    String json = gson2.toJson(inventory);
-                    editor.putString("user_inventory", json); // Storing string
-                    editor.commit();
+                    System.out.println(anItem.getQuantity());
+                    isAClone = true;
                 }
             }
+            if(!isAClone) {
+                InventoryItem newItem = new InventoryItem();
+                newItem.setCardInfo(currentCard);
+                newItem.setQuantity(quantity);
+                inventory.add(newItem);
+            }
+            SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
+            SharedPreferences.Editor editor = pref.edit();
+            Gson gson2 = new Gson();
+            String json = gson2.toJson(inventory);
+            editor.putString("user_inventory", json); // Storing string
+            editor.commit();
         }
     }
 
