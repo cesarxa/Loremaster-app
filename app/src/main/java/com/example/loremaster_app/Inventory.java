2a59f8740902ba23dc6ba4f6b720786a42e63f58
JayWheels <111hotwheels@gmail.com>
2020-07-17 14:28:13 -0600

    If the card is found in your inventory, it is now displayed.

--- app/src/main/java/com/example/loremaster_app/Inventory.java
+++ app/src/main/java/com/example/loremaster_app/Inventory.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,10 +25,14 @@ public class Inventory extends AppCompatActivity {
     List<InventoryItem> inventory;
     Integer quantity;
     EditText getCard;
+    ImageView imageViewDisplayer;
+    TextView textViewDisplayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+        imageViewDisplayer = (ImageView) findViewById(R.id.imageView2);
+        textViewDisplayer = (TextView) findViewById(R.id.textView);
         getCard = (EditText) findViewById(R.id.searchPrefInventory);
         inventory = new ArrayList<InventoryItem>();
         quantity = 1;
@@ -54,10 +62,24 @@ public class Inventory extends AppCompatActivity {
                 System.out.println("WE GOT INSIDE THE IF STATEMENT");
                 currentCard = anItem.getCardInfo();
                 currentCard.displayAll();
+                showCard();
             }
         }
     }
 
+    public void showCard(){
+        String murl = currentCard.getImageURIs().get("border_crop"); //newCard.getLargeImage(); // need to add method or url of images searched;
+        Picasso.get().load(murl).into(imageViewDisplayer);
+
+        String cInfo =  ("Name: " + currentCard.getName()) +
+                //("\nOracle Text: " + newCard.getOracleText()) +
+                //("\nType: " + newCard.getTypeLine()) +
+                //("\nMana Cost: " + newCard.getManaCost()) +
+                ("\nPrice USD: $" + currentCard.getPrices().get("usd")) +
+                ("\nPrice USD Foil: $" + currentCard.getPrices().get("usd_foil"));
+        textViewDisplayer.setText(cInfo);
+        textViewDisplayer.setMovementMethod(new ScrollingMovementMethod());
+    }
 
     @Override
     public void finish() {
